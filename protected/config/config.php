ad7e270963846785daed6c4c98ec6db0d827b5d0
Hexality <hexalitymusic@gmail.com>
2021-03-16 23:58:17 -0300

    Update config.php

--- protected/config/config.php
+++ protected/config/config.php
@@ -20,7 +20,7 @@ define('TWITTER_CARDS', false);
 
 // If you're using this, make sure to put a forward slash before.
 // E.g. "/imageserve" not "imageserve"
-define('IMAGESERVE_DIR', '');
+define('IMAGESERVE_DIR', getenv('IMAGESERVE_DIR'));
 define('TWITTER_HANDLE', '@aerouk_');
 
 define('APP_NAME', 'ImageServe-Heroku');
