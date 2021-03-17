1ce3a712a3c85286db2c3d1b55982cfb7614f781
Hexality <hexalitymusic@gmail.com>
2021-03-17 00:24:55 -0300

    Update viewer.php

--- viewer.php
+++ viewer.php
@@ -21,7 +21,7 @@ if ($index) {
 $type = $_GET['type'];
 $file = $_GET['file'];
 
-$filelocation = __DIR__ . "/images/$type/$file.$type";
+$filelocation = __DIR__ . "/media/$type/$file.$type";
 
 if ( ! file_exists(realpath($filelocation)) || ! array_key_exists($type, $types)) {
     header('HTTP/1.0 404 Not Found');
