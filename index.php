bc53279063dd21807fce4e3000a2cd7dd9479ce2
Hexality <hexalitymusic@gmail.com>
2021-03-19 04:07:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
-<?php
-
-print "\n";
-print "<!DOCTYPE html>\n";
-print "<html>\n";
-print "	<head>\n";
-print "		<title>( ͡° ͜ʖ ͡°)</title>\n";
-print "		<style>\n";
-print "			.video {\n";
-print "				position: fixed;\n";
-print "				right: 0;\n";
-print "				bottom: 0;\n";
-print "				min-width: 100%;\n";
-print "				min-height: 100%;\n";
-print "			}\n";
-print "		</style>\n";
-print "	</head>\n";
-print "	<body>\n";
-print "		<center>\n";
-print "			<video autoplay loop class=\"video\">\n";
-print "				<source src=\"https://i.fiery.me/i2mzf.mp4\" type=\"video/mp4\">\n";
-print "			</video>\n";
-print "			<script>\n";
-print "				document.addEventListener(\"DOMContentLoaded\", function(event) {\n";
-print "			  		document.querySelector(\"video.video\").play();\n";
-print "				});\n";
-print "			</script>\n";
-print "		</center>\n";
-print "	</body>\n";
-print "</html>";
-
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>( ͡° ͜ʖ ͡°)</title>
+		<style>
+			.video {
+				position: fixed;
+				right: 0;
+				bottom: 0;
+				min-width: 100%;
+				min-height: 100%;
+			}
+		</style>
+	</head>
+	<body>
+		<center>
+			<video autoplay loop class="video">
+				<source src="https://i.fiery.me/i2mzf.mp4" type="video/mp4">
+			</video>
+			<script>
+				document.addEventListener("DOMContentLoaded", function(event) {
+			  		document.querySelector("video.video").play();
+				});
+			</script>
+		</center>
+	</body>
+</html>
